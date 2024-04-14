669c31cbda3e5c70205db8645dc1218f43d04f68
Ikramzhan <153011645+Ikramzhan@users.noreply.github.com>
2024-04-15 00:11:51 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
+import time
 import telebot 
 from time import sleep
 
+import logging
+
 import random
 
+
+
 TOKEN = '5875708806:AAED_HXh60M9XFWh6Jy-30JsAfQe-Uo6DK0'
+
 bot = telebot.TeleBot(TOKEN)
 
 
@@ -24,5 +30,17 @@ def start_message(message):
 def text(message):
        if message.text.lower() == "салам":
           bot.send_message(message.chat.id,'Салам, салам ')
+       
+       elif message.text.lower() == "атыңыз ким?":
+        bot.send_message(message.chat.id,'Бектурхан')
+
+       elif message.text.lower() == "атаңдын аты ким?":
+        bot.send_message(message.chat.id,'Икрам') 
+
+       elif message.text.lower() == "энеңдин аты ким?":
+        bot.send_message(message.chat.id,'Зарифа') 
+         
+       elif message.text.lower() == "эжеңдин аты ким?":
+        bot.send_message(message.chat.id,'Мавлида')       
 
 bot.polling()
